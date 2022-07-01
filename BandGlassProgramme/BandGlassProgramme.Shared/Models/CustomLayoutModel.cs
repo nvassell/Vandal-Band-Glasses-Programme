39b0cb1dbafa8b17f632bee192dc6b765feeb18a
Nathan Vassell <36028839+nvassell@users.noreply.github.com>
2022-06-30 21:36:25 -0700

    Add default and custom models

---
+++ BandGlassProgramme/BandGlassProgramme.Shared/Models/CustomLayoutModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace BandGlassProgramme.Models
+{
+    public class CustomLayoutModel
+    {
+        public List<LayoutDetail> layoutDetails { get; init; }
+    }
+
+    public class LayoutDetail
+    {
+        public string participant { get; init; }
+        public string color { get; init; }
+        public int xPos { get; init; }
+        public int yPos { get; init; }
+    }
+}
---
+++ BandGlassProgramme/BandGlassProgramme.Shared/Models/DefaultLayoutModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace BandGlassProgramme.Models
+{
+    public class DefaultLayoutModel
+    {
+        public List<string> participantNames { get; init; }
+        public int numRows { get; init; }
+        public int numColumns { get; init; }
+        public string customLayoutDirectory { get; init; } = "./Custom_Layouts/";
+    }
+}
